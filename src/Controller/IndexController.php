dd0f048ccac12ef817468f29db18c5e128c57c33
Xatenev <xatenev@gmail.com>
2021-08-24 05:24:46 +0200

    fix undefined tar/gz/password variables

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,19 +8,26 @@ use Slim\Views\PhpRenderer;
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $renderer = new PhpRenderer(TEMPLATE_DIR);
-    return $renderer->render($response, 'index.php', ['settings' => false]);
+
+    return $renderer->render($response, 'index.php', [
+        'tar' => false,
+        'gz' => false,
+        'password' => false,
+        'share' => false,
+        'settings' => false
+    ]);
 });
 
 $app->get('/{route}', function (Request $request, Response $response, $args) {
     $renderer = new PhpRenderer(TEMPLATE_DIR);
 
     $route = $args['route'];
+    
     $view = [];
     $view['tar'] = str_contains($route, 'tar');
     $view['gz'] = str_contains($route, 'tar') && str_contains($route, 'gz');
     $view['password'] = str_contains($route, 'password');
     $view['share'] = str_contains($route, 'share');
-
     $view['settings'] = in_array(true, $view);
 
     return $renderer->render($response, 'index.php', $view);
