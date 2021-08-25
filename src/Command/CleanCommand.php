798db027822a55adafb3385a7872b77d25590d76
Xatenev <xatenev@gmail.com>
2021-08-25 09:23:37 +0200

    Add clean/start composer commands, add strict types

---
+++ src/Command/CleanCommand.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Xatenev\Zippify\Command;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+use FilesystemIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+/**
+ * Cleans all upload and data folders
+ *
+ * Example:
+ * `composer clean`
+ */
+class CleanCommand
+{
+    const WHITELIST = [
+        'gitkeep'
+    ];
+
+    public static function clean(Event $event)
+    {
+        require_once __DIR__ . '/../const.php';
+
+        // Remove /upload folder
+        $directoryIterator = new RecursiveDirectoryIterator(UPLOAD_DIR, FilesystemIterator::SKIP_DOTS);
+        $recursiveIterator = new RecursiveIteratorIterator($directoryIterator, RecursiveIteratorIterator::CHILD_FIRST);
+        foreach ($recursiveIterator as $file) {
+            $fullPath = $file->getPath() . DS . $file->getFilename();
+
+            if($file->isDir()) {
+                rmdir($fullPath);
+            } else {
+                if(!in_array($file->getExtension(), self::WHITELIST, true)) {
+                    unlink($fullPath);
+                }
+            }
+        }
+
+        // Remove /out folder
+        $directoryIterator = new RecursiveDirectoryIterator(OUT_DIR, FilesystemIterator::SKIP_DOTS);
+        $recursiveIterator = new RecursiveIteratorIterator($directoryIterator, RecursiveIteratorIterator::CHILD_FIRST);
+        foreach ($recursiveIterator as $file) {
+            $fullPath = $file->getPath() . DS . $file->getFilename();
+
+            if($file->isDir()) {
+                rmdir($fullPath);
+            } else {
+                if(!in_array($file->getExtension(), self::WHITELIST, true)) {
+                    unlink($fullPath);
+                }
+            }
+        }
+    }
+}
---
+++ src/Command/StartCommand.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Xatenev\Zippify\Command;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+/**
+ * Starts the application
+ *
+ * Example:
+ * `composer start -- 9999`
+ */
+class StartCommand
+{
+    public static function start(Event $event)
+    {
+        require_once __DIR__ . '/../const.php';
+
+        $port = $event->getArguments()[0];
+        exec('php -S localhost:' . $port . ' -t ' . PUBLIC_DIR);
+    }
+}
