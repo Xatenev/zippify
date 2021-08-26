dbab3411f8a066ff551c935ff5b77ccbd022dc3a
Xatenev <xatenev@gmail.com>
2021-08-26 09:53:12 +0200

    Responsive design

--- src/Template/index.php
+++ src/Template/index.php
@@ -3,7 +3,8 @@
     <title>Zippify</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
-    <meta name="description" content="Zippify">
+    <meta name="description" content="Zippify - Generate zip/tar archives from files">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= BASE_URL . 'img/favicon.png' ?>">
     <script src="../../vendor/js/micromodal.js"></script>
     <script src="../../vendor/js/dropzone.js"></script>
     <script src="../../js/dropzone_options.js"></script>
@@ -14,6 +15,7 @@
     <link rel="stylesheet" href="../../css/dropzone.css">
     <link rel="stylesheet" href="../../css/loading.css">
     <link rel="stylesheet" href="../../css/main.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
     <script src="https://kit.fontawesome.com/9843e0173c.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Inter:300,300italic,200,200italic,400,400italic%7CPoppins:200,200italic,400,400italic%7CManrope:300,300italic"
           rel="stylesheet" type="text/css">
@@ -22,55 +24,57 @@
 <section id="main">
     <div class="inner">
         <div class="container">
-            <div class="wrapper">
-                <div class="inner">
-                    <h3>Zip all your files instantly</h3>
-                    <h1>Zippify</h1>
-                    <form action="/upload" class="dropzone needsclick dz-clickable <?= $settings ? 'expanded' : '' ?>" id="zippify-upload">
-                        <div class="dz-message needsclick">
-                            <button type="button" class="dz-button">Drop files here or click to upload.</button>
-                        </div>
+            <div class="inner">
+                <h3>Zip all your files instantly</h3>
+                <h1>Zippify</h1>
+                <form action="/upload" class="dropzone needsclick dz-clickable <?= $settings ? 'expanded' : '' ?>"
+                      id="zippify-upload">
+                    <div class="dz-message needsclick">
+                        <button type="button" class="dz-button">Drop files here or click to upload</button>
+                    </div>
 
-                        <div class="settings-menu <?= $settings ? '' : 'hidden' ?>">
-                            <hr>
-                            <div class="checkbox-group">
+                    <div class="settings-menu <?= $settings ? '' : 'hidden' ?>">
+                        <hr>
+                        <div class="checkbox-group">
+                            <div>
                                 <div>
-                                    <div>
-                                        <input type="checkbox" name="settings" id="settings-tar" <?= $tar ? 'checked' : '' ?>>
-                                        <label for="settings-tar">tar<i class="fas fa-file-archive"></i></label>
-                                    </div>
-                                    <div>
-                                        <input type="checkbox" name="settings" id="settings-gz" <?= $gz ? 'checked' : '' ?>>
-                                        <label for="settings-gz">.gz compression<i class="fas fa-archive"></i></label>
-                                    </div>
-                                    <div class="password-input <?= $password ? '' : 'hidden' ?>">
-                                        <input type="text" id="settings-password-input" name="password">
-                                    </div>
+                                    <input type="checkbox" name="settings"
+                                           id="settings-tar" <?= $tar ? 'checked' : '' ?>>
+                                    <label for="settings-tar">tar<i class="fas fa-file-archive"></i></label>
                                 </div>
                                 <div>
-                                    <div>
-                                        <input type="checkbox" name="settings" id="settings-password" <?= $password ? 'checked' : '' ?>>
-                                        <label for="settings-password">password<i class="fas fa-lock"></i></label>
-                                    </div>
-                                    <div>
-                                        <input type="checkbox" name="settings" id="settings-share" <?= $share ? 'checked' : '' ?>>
-                                        <label for="settings-share">share<i class="fas fa-share-alt"></i></label>
-                                    </div>
+                                    <input type="checkbox" name="settings" id="settings-gz" <?= $gz ? 'checked' : '' ?>>
+                                    <label for="settings-gz">.gz<i class="fas fa-archive"></i></label>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <input type="checkbox" name="settings"
+                                           id="settings-password" <?= $password ? 'checked' : '' ?>>
+                                    <label for="settings-password">password<i class="fas fa-lock"></i></label>
+                                </div>
+                                <div class="password-input <?= $password ? '' : 'hidden' ?>">
+                                    <input type="text" id="settings-password-input" name="password">
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="settings"
+                                           id="settings-share" <?= $share ? 'checked' : '' ?>>
+                                    <label for="settings-share">share<i class="fas fa-share-alt"></i></label>
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="settings open <?= $settings ? 'close' : '' ?> hvr-pulse-grow">
-                            <i class="fas <?= $settings ? 'fa-times' : 'fa-wrench' ?>"></i>
-                        </div>
+                    <div class="settings open <?= $settings ? 'close' : '' ?> hvr-pulse-grow">
+                        <i class="fas <?= $settings ? 'fa-times' : 'fa-wrench' ?>"></i>
+                    </div>
 
-                        <div class="loading">
-                            <div class="line"></div>
-                            <div class="subline inc"></div>
-                            <div class="subline dec"></div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="loading">
+                        <div class="line"></div>
+                        <div class="subline inc"></div>
+                        <div class="subline dec"></div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
